92bb9dd60254440fa5ee2f3e564fcb60a4cbf5f7
Abhishek <abhishekc838@gmail.com>
2024-01-09 23:13:40 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const heading = React.createElement("h1", {class:"test"}, [ 
+    React.createElement("div", { id: "child"},[
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag")
+    ]),
+    React.createElement("div", { id: "child2"},[
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag")
+    ])
+])
+console.log(heading);
+root.render(heading)
