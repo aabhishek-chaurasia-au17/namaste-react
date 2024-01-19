39d31b309ea0cae3214e650e2f0429c6d127c139
Abhishek <abhishekc838@gmail.com>
2024-01-19 21:33:19 +0530

    learning chaptor 2

--- Chapter 01 - Inception/coding/App.js
+++ Chapter 01 - Inception/coding/App.js
@@ -1,14 +1,19 @@
-const root = ReactDOM.createRoot(document.getElementById("root"))
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {class:"test"}, [ 
-    React.createElement("div", { id: "child"},[
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag")
-    ]),
-    React.createElement("div", { id: "child2"},[
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag")
-    ])
-])
+const heading = React.createElement("h1", { className: "test" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag")
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag")
+  ])
+]);
+
 console.log(heading);
-root.render(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 🚀
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Spiltting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
