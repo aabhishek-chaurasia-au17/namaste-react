29221a287bf9cee84dceede4f37e0e6fe34dda34
Abhishek <abhishekc838@gmail.com>
2024-03-05 00:03:20 +0530

    learning cheapter 2

--- Chapter 01 - Inception/coding/App.js
+++ Chapter 01 - Inception/coding/App.js
@@ -6,6 +6,17 @@
 
 // Manipulate the HTML DOM using React
 
+const parent = React.createElement("div", {id: "parent"}, [
+  React.createElement("div", {id: "child"}, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag")
+  ]),
+  React.createElement("div", {id: "child2"}, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag")
+  ])
+])
+
 // Create nested React Elements
 const heading = React.createElement(
   "h1",
--- Chapter 02 - Igniting our App/Coding/App.js
+++ Chapter 02 - Igniting our App/Coding/App.js
@@ -25,29 +25,30 @@
  * Transitive Dependencies
  */
 
-
 // imported react and reactdom from nodemodule folder
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading"
-  );
-  const heading1 = React.createElement(
     "h1",
     {
       id: "title",
     },
     "heading1"
   );
+  const heading1 = React.createElement(
+    "h2",
+    {
+      id: "title",
+      className: "test"
+    },
+    "heading2"
+  );
   
   const container = React.createElement(
     "div",
     {
-      id: "container",
+      id: "container"
     },
     [heading, heading1]
   );
