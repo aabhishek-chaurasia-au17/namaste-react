e6c28f8d41a382a94b7f45cf3b0f5458f90c8520
Abhishek <abhishekc838@gmail.com>
2024-03-22 00:13:21 +0530

    added notes learn useState

--- Chapter 05 - Let's get Hooked!/Coding/src/components/Body.js
+++ Chapter 05 - Let's get Hooked!/Coding/src/components/Body.js
@@ -1,12 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mokeData"
+import { useState } from "react";
 
 const Body = () => {
+    const [resList, setResList] = useState(restaurantList)
+
+    function getResFilter() {
+        setResList(
+            resList.filter((item) => {
+                return item.data.avgRating > 4
+            })
+        )
+    }
+
     return (
         <div className="body">
-            <div className="Search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={getResFilter}>Top Rated Restaurants</button>
+            </div>
             <div className="res-container">
-                <RestaurantCard resData={restaurantList} />
+            {resList.map((resList) => (
+                <RestaurantCard key={resList.data.id} resData={resList} />
+            ))}
             </div>
         </div>
     )
